d31d6c2b804137cb362ad1e9f4ee4b49d8c20c96
Abdellah Alaoui <als.abdellah@gmail.com>
2017-10-04 11:23:57 +0000

    use import instead of require

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
-const express = require('express')
+import express from "express"
+
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+res.send('Hello World!')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+console.log('Example app listening on port 3000!')
 })
