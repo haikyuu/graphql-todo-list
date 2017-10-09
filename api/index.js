f72e96f41dd518532a04313b393c2259b7716c3a
Abdellah Alaoui <als.abdellah@gmail.com>
2017-10-09 21:55:38 +0100

    use cors library to enable communicating with client

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import schema from "./presentation/schema";
@@ -6,6 +7,21 @@ import Todo from "./business/todo";
 const PORT = 3000;
 
 const app = express();
+app.use(cors());
+const corsOptions = {
+  origin(origin, callback) {
+    callback(null, true);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.use(allowCrossDomain);
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
